24ab9b00a9b370aebcdf9761d5707cfa4956097f
Joshua Byrd <phocks@gmail.com>
2022-11-04 17:00:05 +1000

    Whooooa we're half way there

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,40 @@
-const React = require('react');
-const Scrollyteller = require('@abcnews/scrollyteller');
-const Map = require('./Map');
-
-const styles = require('./App.scss');
+const React = require("react");
+const Scrollyteller = require("@abcnews/scrollyteller");
+const Map = require("./Map");
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      property: 'marriagelegal',
-      legend: 'all'
+      property: "marriagelegal",
+      legend: "all",
     };
   }
 
   render() {
     const { data, scrollyteller } = this.props;
 
-    const key = Object.keys(data).filter(key => {
-      return key.replace(/[^a-zA-Z]+/g, '') === this.state.property;
+    const key = Object.keys(data).filter((key) => {
+      return key.replace(/[^a-zA-Z]+/g, "") === this.state.property;
     })[0];
     const p = data[key];
     const colour = p.theme[Object.keys(p.theme)[0]];
 
     return (
-      <div className={styles.base}>
-        <Scrollyteller
-          panels={scrollyteller.panels}
-          className={`Block is-richtext is-piecemeal ${styles.scrollyteller}`}
-          panelClassName="Block-content u-layout u-richtext"
-          onMarker={config =>
-            this.setState(state => ({ property: config.key, legend: config.legend || state.legend }))
-          }>
-          <Map data={p} legend={this.state.legend} />
-        </Scrollyteller>
-      </div>
+      <Scrollyteller
+        panels={scrollyteller.panels}
+        className={`Block is-richtext is-piecemeal`}
+        panelClassName="Block-content u-layout u-richtext"
+        onMarker={(config) =>
+          this.setState((state) => ({
+            property: config.key,
+            legend: config.legend || state.legend,
+          }))
+        }
+      >
+        <Map data={p} legend={this.state.legend} />
+      </Scrollyteller>
     );
   }
 }
